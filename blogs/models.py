0452616891397f618d086e7091d0dae6fd9ef962
Vladislav <166797675+le-shien@users.noreply.github.com>
2024-05-17 04:03:51 +0300

    Add files via upload

---
+++ blogs/models.py
@@ -0,0 +1,37 @@
+from dataclasses import dataclass
+from datetime import datetime
+
+
+@dataclass
+class Post:
+    title: str
+    text: str
+
+
+class Blog:
+    def __init__(
+            self,
+            author: str,
+            email: str,
+    ):
+        self.author = author
+        self.email = email
+        self._posts = []
+
+    def add_post(self, post: Post):
+        self._posts.append(post)
+
+    def get_all_posts(self):
+        return self._posts
+
+
+if __name__ == '__main__':
+    blog = Blog(author='Vlad', email='vlad@gaz.ru')
+    while True:
+        post = Post(
+            title=input('Название >'),
+            text=input('Текст >'),
+        )
+        blog.add_post(post)
+        for index, post in enumerate(blog.get_all_posts()):
+            print(f'Пост {index + 1}: {post}')
---
+++ blogs/repository.py
@@ -0,0 +1,40 @@
+from models import Post
+
+SEPARATOR = ';'
+
+
+def save_post(post: Post) -> None:
+    row = [post.title, post.text]
+    bar = SEPARATOR.join(row)
+    with open('database.txt', 'a', encoding='utf-8') as file:
+        file.write(bar + '\n')
+
+
+def load_posts() -> list[Post]:
+    with open('database.txt', 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+    posts = []
+    for line in lines:
+        line_list = line.strip().split(SEPARATOR)
+        post = Post(line_list[0], line_list[1])
+        posts.append(post)
+    return posts
+
+
+if __name__ == '__main__':
+    # post = Post('Just post', 'saved post')
+    # save_post(post)
+    posts = load_posts()
+    print(*posts, sep='\n')
+    # a = ['bobik', '2', 'dog']
+    # b = '//'.join(a)
+    # print(type(b))
+    # print(b)
+    # with open('database.txt', 'a', encoding='utf-8') as file:
+    #     file.write(b + '\n')
+    # with open('database.txt', 'r', encoding='utf-8') as file:
+    #     lines = file.readlines()
+    # data = []
+    # for line in lines:
+    #     data.append(line.strip().split('//'))
+    # print(data)
